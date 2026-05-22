1689c558430d475522cfc19fa0f50816fd0e87a4
Mihai3636 <mihai3636@email.com>
2026-05-22 16:25:33 +0300

    Create reusable carousel component css and js

--- js/index.js
+++ js/index.js
@@ -6,12 +6,18 @@ const navListEl = document.getElementById("navList");
 
 const mobileNav = window.matchMedia("(max-width: 47.9375rem)");
 
+const carouselBtnsListEl = document.querySelector(".carousel__btns");
+
+const carouselItems = [...document.querySelectorAll(".carousel__item")];
+const carouselBtns = [...document.querySelectorAll(".carousel__btn")];
+
 console.log(btnMenuEl);
 
 mobileNav.addEventListener("change", syncNavState);
 
 syncNavState();
 setupTabTrapping();
+initCarouselBtns();
 
 btnMenuEl.addEventListener("click", () => {
   const currentState = navEl.dataset.open == "true";
@@ -107,5 +113,47 @@ function syncNavState() {
     navListEl.removeAttribute("inert");
     navListEl.setAttribute("aria-hidden", "false");
     document.body.classList.remove("overflow-hidden");
+  }
+}
+
+function initCarouselBtns() {
+  if (carouselBtnsListEl == null) return;
+
+  carouselBtnsListEl.addEventListener("click", (ev) => {
+    console.log(ev.target, ev.currentTarget);
+
+    const target = ev.target;
+    const carouselBtnEl = ev.target.closest(".carousel__btn");
+
+    if (carouselBtnEl == null) return;
+
+    console.log("Clicked ", carouselBtnEl.dataset.index);
+
+    const clickedIndex = Number(carouselBtnEl.dataset.index);
+    updateCarouselBtns(clickedIndex);
+    updateCarouselItems(clickedIndex);
+  });
+}
+
+function updateCarouselItems(indexClicked) {
+  let currentIndex = 0 - indexClicked;
+
+  carouselItems.forEach((item) => {
+    item.dataset.index = currentIndex;
+    currentIndex += 1;
+  });
+
+  console.log(carouselItems);
+}
+
+function updateCarouselBtns(indexClicked) {
+  for (let i = 0; i < carouselBtns.length; i++) {
+    const btn = carouselBtns[i];
+    if (Number(btn.dataset.index) === indexClicked) {
+      carouselBtns[i].setAttribute("data-selected", "");
+      continue;
+    }
+
+    btn.removeAttribute("data-selected");
   }
 }
